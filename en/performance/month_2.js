e35b0ee12071f47efd6097ade3168e7c7b0daba8
framebrace <songgmo@naver.com>
2023-04-10 16:43:29 +0900

    0406 수정내용 적용

--- en/performance/month_2.js
+++ en/performance/month_2.js
@@ -3,77 +3,22 @@ let schedule = [
 		day: '3',
 		week: '금 FRI',
 		category: 'Exchange',
-		title: 'National Gugak Center Exchange performance <Chukwon, the Prayer> ',
-		imgSrc: '../../asset/images/performance/thumb/0601.jpg',
-		inTit1: '2. 3.(Fri) 19:30',
-		inTit2: 'Yeonak-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0601.html'
+		title: '국립국악원 교류공연 <축원> National Gugak Center Exchange performance <Chukwon, the Prayer>',
+		imgSrc: '0107.jpg',
+		inTit1: '1월 7일, 14일, 28일, 매주 토요일 15:00',
+		inTit2: '연악당',
+		inTit3: 'S석 10,000원 A석 8,000원',
+		link: '0107.html'
 	},
 	{
 		day: '4',
 		week: '토 SAT',
 		category: 'Permanent',
-		title: 'Saturday Concert <2023 Saturday performance of Korean music and dance>',
-		imgSrc: '../../asset/images/performance/thumb/0401.jpg',
-		inTit1: '2. 4. ~ 9. 23. Every Saturday 15:00',
-		inTit2: 'Yeji-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0401.html'
-	},
-	{
-		day: '11',
-		week: '토 SAT',
-		category: 'Permanent',
-		title: 'Saturday Concert <2023 Saturday performance of Korean music and dance>',
-		imgSrc: '../../asset/images/performance/thumb/0401.jpg',
-		inTit1: '2. 4. ~ 9. 23. Every Saturday 15:00',
-		inTit2: 'Yeji-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0401.html'
-	},
-	{
-		day: '17',
-		week: '금 FRI',
-		category: 'Exchange',
-		title: 'Jindo National Gugak Center Exchange performance <The Island>',
-		imgSrc: '../../asset/images/performance/thumb/0602.jpg',
-		inTit1: '2. 17.(Fri) 19:30',
-		inTit2: 'Yeonak-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0602.html'
-	},
-	{
-		day: '18',s
-		week: '토 SAT',
-		category: 'Permanent',
-		title: 'Saturday Concert <2023 Saturday performance of Korean music and dance>',
-		imgSrc: '../../asset/images/performance/thumb/0401.jpg',
-		inTit1: '2. 4. ~ 9. 23. Every Saturday 15:00',
-		inTit2: 'Yeji-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0401.html'
-	},
-	{
-		day: '18',
-		week: '금 FRI',
-		category: 'Exchange',
-		title: 'Jindo National Gugak Center Exchange performance <The Island>',
-		imgSrc: '../../asset/images/performance/thumb/0602.jpg',
-		inTit1: '2. 18.(Sat) 17:00',
-		inTit2: 'Yeonak-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0602.html'
-	},
-	{
-		day: '25',
-		week: '토 SAT',
-		category: 'Permanent',
-		title: 'Saturday Concert <2023 Saturday performance of Korean music and dance>',
-		imgSrc: '../../asset/images/performance/thumb/0401.jpg',
-		inTit1: '2. 4. ~ 9. 23. Every Saturday 15:00',
-		inTit2: 'Yeji-dang Hall',
-		inTit3: 'A seat 10,000won, B seat 8,000won',
-		link: '0401.html'
+		title: '토요상설 <토요신명 2023> Saturday Concert <2023 Saturday performance of Korean music and dance>',
+		imgSrc: '0107.jpg',
+		inTit1: '1월 7일, 14일, 28일, 매주 토요일 15:00',
+		inTit2: '연악당',
+		inTit3: 'S석 10,000원 A석 8,000원',
+		link: '0204.html'
 	},
 ];
